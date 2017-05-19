f729df19a8a452f3858537e418a641e40a6625fa
Dave Honneffer <pearofducks@gmail.com>
2017-05-19 14:56:11 +0200

    refactor

--- src/App.vue
+++ src/App.vue
@@ -5,36 +5,34 @@
       <h2>Persona</h2>
     </header>
     <main>
-      <article class="element image">
+      <article class="element image column">
         <image-upload></image-upload>
         <multiline :r="2" ph="Persona quote - should capture the essence of this persona's personality"></multiline>
       </article>
       <article class="element stats">
-        <div class="stats-block">
-          <div class="tabular row">
-            <h4>Age</h4>
-            <input type="text" placeholder="1-105">
-          </div>
-          <div class="tabular row">
-            <h4>Occupation</h4>
-            <input type="text" placeholder="What they do for a living">
-          </div>
-          <div class="tabular row">
-            <h4>Marital Status</h4>
-            <input type="text" placeholder="Single / Married / Other">
-          </div>
-          <div class="tabular row">
-            <h4>Location</h4>
-            <input type="text" placeholder="Where they live">
-          </div>
-          <div class="tabular row">
-            <h4>Tier</h4>
-            <input type="text" placeholder="Enthusiast / Prosumer / Pro">
-          </div>
-          <div class="tabular row">
-            <h4>Archetype</h4>
-            <input type="text" placeholder="Character model">
-          </div>
+        <div class="tabular row">
+          <h4>Age</h4>
+          <input type="text" placeholder="1-105">
+        </div>
+        <div class="tabular row">
+          <h4>Occupation</h4>
+          <input type="text" placeholder="What they do for a living">
+        </div>
+        <div class="tabular row">
+          <h4>Marital Status</h4>
+          <input type="text" placeholder="Single / Married / Other">
+        </div>
+        <div class="tabular row">
+          <h4>Location</h4>
+          <input type="text" placeholder="Where they live">
+        </div>
+        <div class="tabular row">
+          <h4>Tier</h4>
+          <input type="text" placeholder="Enthusiast / Prosumer / Pro">
+        </div>
+        <div class="tabular row">
+          <h4>Archetype</h4>
+          <input type="text" placeholder="Character model">
         </div>
       </article>
       <article class="element brands">
@@ -68,7 +66,7 @@
           <li><multiline ph="Frustration 3"></multiline></li>
         </ul>
       </article>
-      <article class="element technology">
+      <article class="element technology column">
         <h3>Technology</h3>
         <div class="row">
           <strength-gauge title="IT & Internet"></strength-gauge>
@@ -79,7 +77,7 @@
           <strength-gauge title="Social Networks"></strength-gauge>
         </div>
       </article>
-      <article class="element motivations">
+      <article class="element motivations column">
         <h3>Motivations</h3>
         <strength-gauge title="Incentive"></strength-gauge>
         <strength-gauge title="Fear"></strength-gauge>
@@ -88,44 +86,12 @@
         <strength-gauge title="Power"></strength-gauge>
         <strength-gauge title="Social"></strength-gauge>
       </article>
-      <article class="element personality">
+      <article class="element personality column">
         <h3>Personality</h3>
-        <div class="vertical row">
-          <div class="spread row">
-            <h4>Introvert</h4>
-            <h4>Extrovert</h4>
-          </div>
-          <div class="slider">
-            <input type="range" value="5" min="0" max="10"> 
-          </div>
-        </div>
-        <div class="vertical row">
-          <div class="spread row">
-            <h4>Intuition</h4>
-            <h4>Sensing</h4>
-          </div>
-          <div class="slider">
-            <input type="range" value="5" min="0" max="10"> 
-          </div>
-        </div>
-        <div class="vertical row">
-          <div class="spread row">
-            <h4>Feeling</h4>
-            <h4>Thinking</h4>
-          </div>
-          <div class="slider">
-            <input type="range" value="5" min="0" max="10"> 
-          </div>
-        </div>
-        <div class="vertical row">
-          <div class="spread row">
-            <h4>Judging</h4>
-            <h4>Perceiving</h4>
-          </div>
-          <div class="slider">
-            <input type="range" value="5" min="0" max="10"> 
-          </div>
-        </div>
+        <balance-gauge left="Introvert" right="Extrovert"></balance-gauge>
+        <balance-gauge left="Intuition" right="Sensing"></balance-gauge>
+        <balance-gauge left="Feeling" right="Thinking"></balance-gauge>
+        <balance-gauge left="Judging" right="Perceiving"></balance-gauge>
       </article>
     </main>
   </div>
@@ -135,9 +101,10 @@
 import multiline from './multiline.vue'
 import imageUpload from './imageUpload.vue'
 import strengthGauge from './strengthGauge.vue'
+import balanceGauge from './balanceGauge.vue'
 export default {
   name: 'app',
-  components: { multiline, imageUpload, strengthGauge }
+  components: { multiline, imageUpload, strengthGauge, balanceGauge }
 }
 </script>
 
@@ -201,104 +168,47 @@ article.element
     grid-row: 3 / 5
   h3
     color: #228ba8
-
-.row
-  display: flex
-  align-items: center
-
-// .image
-//   display: flex
-//   flex-direction: column
-
-.personality, .image
-  display: flex
-  flex-direction: column
-  justify-content: space-between
-  > textarea
-    flex: 1
-    padding-top: 1.2rem
-  .row
-    margin: 0.3rem 0
-  .vertical.row
-    flex-direction: column
-    align-items: stretch
-  .spread.row
-    justify-content: space-between
   h4
     font-size: 1.2rem
     margin-bottom: 0.6rem
-  .slider
-    flex: 2
-    input[type='range']
-      -webkit-appearance: none;
-      position: relative;
-      overflow: hidden;
-      height: 1.2rem;
-      width: 100%;
-      cursor: pointer;
-      border-radius: 1.2rem;
-      &::-webkit-slider-runnable-track
-        background: #dadfe4
-      &::-webkit-slider-thumb
-        -webkit-appearance: none
-        width: 3.6rem
-        height: 1.2rem
-        background: #a7b3c2
-        border-radius: 4.8rem
 
-.motivations, .technology
+.column
   display: flex
   flex-direction: column
+.row
+  display: flex
+  align-items: center
+.1-3
+  flex: 1
+.2-3
+  flex: 2
+
+.personality, .image, .motivations, .technology
   justify-content: space-between
+
+.personality, .image
+  > textarea
+    flex: 1
+    padding-top: 1.2rem
+
+.motivations, .technology
   .row
     flex: 1
     margin: 0.3rem 0
     .row ~ .row
       padding-left: 2.4rem
       margin-left: 2.4rem
-  h4
-    flex: 1
-    font-size: 1.2rem
-    margin-bottom: 0.6rem
-  .slider
-    flex: 2
-    input[type='range']
-      -webkit-appearance: none;
-      position: relative;
-      overflow: hidden;
-      height: 1.2rem;
-      width: 100%;
-      cursor: pointer;
-      border-radius: 0;
-      &::-webkit-slider-runnable-track
-        background: #dadfe4
-      &::-webkit-slider-thumb
-        -webkit-appearance: none
-        width: 0px
-        height: 0px
-        background: #fff
-        box-shadow: -100vw 0 0 100vw #a7b3c2
-        border: 1.2rem solid #a7b3c2
 
-.stats-block
-  h4, p
-    font-size: 1.2rem
-  p
-    flex: 2
+.stats
   h4
     color: #228ba8
     flex: 1
     margin-right: 0.6rem
-.tabular.row
-  margin-bottom: 1.2rem
   input[type="text"]
     flex: 2
-  h4
-    margin-bottom: 0
-
-.slider
-  input
-    width: 100%
+    margin-bottom: 0.6rem
+  .row
+    margin-bottom: 1.2rem
 
 @media print
   #app
--- src/balanceGauge.vue
+++ src/balanceGauge.vue
@@ -1,7 +1,10 @@
 <template>
-  <div class="row">
-    <h4 class="strength-gauge-title">{{ title }}</h4>
-    <div class="strength-gauge">
+  <div class="column">
+    <div class="spread row">
+      <h4>{{left}}</h4>
+      <h4>{{right}}</h4>
+    </div>
+    <div class="slider">
       <input type="range" value="12" min="0" max="24"> 
     </div>
   </div>
@@ -9,9 +12,13 @@
 
 <script>
 export default {
-  name: 'strength-gauge',
+  name: 'balance-gauge',
   props: {
-    title: {
+    left: {
+      type: String,
+      required: true
+    },
+    right: {
       type: String,
       required: true
     }
@@ -19,10 +26,12 @@ export default {
 }
 </script>
 <style lang="sass">
-.strength-gauge-title
-  flex: 1
-.strength-gauge
-  flex: 2
+.column
+  align-items: stretch
+.spread.row
+  justify-content: space-between
+.slider
+  margin: 0.3rem 0
   input[type='range']
     -webkit-appearance: none;
     position: relative;
@@ -30,14 +39,13 @@ export default {
     height: 1.2rem;
     width: 100%;
     cursor: pointer;
-    border-radius: 0;
+    border-radius: 1.2rem;
     &::-webkit-slider-runnable-track
       background: #dadfe4
     &::-webkit-slider-thumb
       -webkit-appearance: none
-      width: 0px
-      height: 0px
-      background: #fff
-      box-shadow: -100vw 0 0 100vw #a7b3c2
-      border: 1.2rem solid #a7b3c2
+      width: 3.6rem
+      height: 1.2rem
+      background: #a7b3c2
+      border-radius: 4.8rem
 </style>
