cb96fc041e4890e8252e1ac28398e718ffffb023
Dave Honneffer <pearofducks@gmail.com>
2017-05-22 12:25:21 +0200

    Inputs in strength-gauges

--- src/strengthGauge.vue
+++ src/strengthGauge.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="row">
-    <h4 class="strength-gauge-title">{{ title }}</h4>
+    <h4 class="strength-gauge-title">
+      <input type="text" :value="title">
+    </h4>
     <div class="strength-gauge">
       <input type="range" value="12" min="0" max="24"> 
     </div>
