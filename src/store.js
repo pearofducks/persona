d3e9617968849e9286e9eddf0cc0adf93528d6c8
Dave Honneffer <pearofducks@gmail.com>
2017-07-02 10:19:08 +0200

    early store

---
+++ src/store.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+const state = {
+  topics: [],
+}
+
+const mutations = {
+}
+
+const actions = {
+}
+
+const getters = {
+}
+
+export default new Vuex.Store({ state, actions, mutations, getters })
