053c3e6cee0bfa511151b6212cdcc8d4ec337e22
Dave Honneffer <pearofducks@gmail.com>
2017-07-02 14:28:21 +0200

    Early store moar

--- src/store.js
+++ src/store.js
@@ -4,7 +4,43 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 const state = {
-  topics: [],
+  fullName: "",
+  personalDetails: {
+    personaQuote: "",
+    age: "",
+    occupation: "",
+    marital: "",
+    location: "",
+    tier: "",
+    archetype: ""
+  },
+  brands: "",
+  bio: "",
+  goals: [],
+  needs: [],
+  frustrations: [],
+  technology: {
+    "IT & Internet": 10,
+    "Mobile Apps": 10,
+    "Software": 10,
+    "Social Networks": 10
+  }
+  motivations: {
+    "Incentive": 10,
+    "Fear": 10,
+    "Achievement": 10,
+    "Growth": 10,
+    "Power": 10,
+    "Social": 10
+  }
+  personality: {
+    ["Introvert", 10, "Extrovert"],
+    ["Intuition", 10, "Sensing"],
+    ["Feeling", 10, "Thinking"],
+    ["Judging", 10, "Perceiving"]
+  }
+
+
 }
 
 const mutations = {
