5132acbaa0bf74daeb486395b3c4e157d55f511e
Dave Honneffer <pearofducks@gmail.com>
2017-07-02 21:05:27 +0200

    Fix compile crap

--- src/store.js
+++ src/store.js
@@ -24,7 +24,7 @@ const state = {
     "Mobile Apps": 10,
     "Software": 10,
     "Social Networks": 10
-  }
+  },
   motivations: {
     "Incentive": 10,
     "Fear": 10,
@@ -32,13 +32,13 @@ const state = {
     "Growth": 10,
     "Power": 10,
     "Social": 10
-  }
-  personality: {
+  },
+  personality: [
     ["Introvert", 10, "Extrovert"],
     ["Intuition", 10, "Sensing"],
     ["Feeling", 10, "Thinking"],
     ["Judging", 10, "Perceiving"]
-  }
+  ]
 
 
 }
